8b20cb0dbbc084da0fb98a541d383254b15aca61
ajey108 <ajayxa1@gmail.com>
2024-08-18 15:46:43 +0530

    Update StikyNotes.jsx

--- src/components/StikyNotes.jsx
+++ src/components/StikyNotes.jsx
@@ -1,31 +1,39 @@
-import React from 'react'
-import { MdOutlineAdd } from 'react-icons/md'
-import { PiPencilSimpleBold } from 'react-icons/pi'
-import { RiDeleteBin3Line } from 'react-icons/ri'
+import React, { useState } from 'react';
+import { PiPencilSimpleBold } from 'react-icons/pi';
+import { RiDeleteBin3Line } from 'react-icons/ri';
 
-const StikyNotes = () => {
+const StikyNotes = ({id,text}) => {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleShow = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const handleSave = ()=>{
+    setIsEdit(!isEdit);
+  }
+
+ 
+
   return (
-
-        <>
-        <div className='bg-gray-800 flex items-center justify-center gap-3 text-white'>
-        <h1 className='p-4'> StiKyNotes</h1>
-        <MdOutlineAdd className='font-extrabold text-4xl' />
-        </div>
+    <>
       
-        <div className="flex justify-center items-center m-4">
-            <textarea className='bg-yellow-100 w-[400px] h-[300px]' name="text" ></textarea>
-            <span><PiPencilSimpleBold/>
-            <RiDeleteBin3Line /></span>
-        </div>
-        
 
-    
+      <div className='flex gap-3 items-center m-4'>
+        <PiPencilSimpleBold onClick={handleShow}  className='text-2xl cursor-pointer' />
+        <RiDeleteBin3Line className='text-2xl cursor-pointer' />
+        <button onClick={handleSave} className='bg-white w-[50px] rounded-sm h-9'>Save</button>
+      </div>
 
-        </>
-  
-
-    
-  )
+      {
+        isEdit && (
+          <div className="m-4 w-[450px]">
+            <textarea className='bg-yellow-100 w-[400px] h-[300px]' name="text"><p>{text}</p></textarea>
+          </div>
+        )
+      }
+    </>
+  );
 }
 
-export default StikyNotes
+export default StikyNotes;
