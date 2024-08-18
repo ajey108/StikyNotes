9a88d711fb799efe3628a9559c71195238ef0411
ajey108 <ajayxa1@gmail.com>
2024-08-18 15:46:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdOutlineAdd } from 'react-icons/md';
 import StikyNotes from './components/StikyNotes'
 
 const App = () => {
+
+  const [notes,setNotes] = useState([]);
+
+  const addNotes =()=>{
+    setNotes([...notes,{id:Date.now(),text:"Click to add text"}])
+  }
+  console.log(notes);
   return (
+    <> 
+    <div className='bg-gray-800 flex items-center justify-center gap-3 text-white'>
+        <h1 className='p-4'>StikyNotes</h1>
+        <MdOutlineAdd onClick={addNotes} className='font-extrabold text-4xl' />
+      </div>
+
     <div>
-      <StikyNotes/>
+      {notes.map((x)=>{
+        return <StikyNotes key={x.id} id={x.id} text={x.text}/>
+      })}
     </div>
+
+    </>
   )
 }
 
